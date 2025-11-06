e84fb1ce13cd95c2e2a6a9f684cc025fcb257379
Ivan Lesnik <ivan.lesnik@moodmedia.hr>
2025-11-06 13:12:08 +0100

    Second Commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,39 @@
+import Home from "./pages/home/Home";
+import Users from "./pages/users/Users";
+import Products from "./pages/products/Products";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+} from "react-router-dom";
 function App() {
 
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <Home />
+    ),
+    },
+    {
+      path: "users",
+      element: <Users/>,
+    }
+    {
+      path: "products",
+      element: <Products/>,
+    }
+  
+  
+  
+  
+  
+  ]);
+
   return (
-    <div>Hello World</div>
+    <div><Home/></div>
   )
 }
 

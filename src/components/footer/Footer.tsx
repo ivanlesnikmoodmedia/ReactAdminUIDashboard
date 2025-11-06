a8728575a9c4d88ba1cef5756abf2a1916d5298b
Ivan Lesnik <ivan.lesnik@moodmedia.hr>
2025-11-06 15:14:16 +0100

    sidebar

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,10 @@
 import "./footer.scss";
 const Footer = () => {
     return (
-        <div className="footer">footer</div>
+        <div className="footer">footer
+          <span>lamadmin</span>
+          <span>$copy Lama Dev Admin Dashboard</span>
+        </div>
     );
 };
 
